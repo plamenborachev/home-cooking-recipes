6d1c04a4bd63db0d37b51457abc4813a1a03a50e
Plamen Borachev <plamen.borachev@gmail.com>
2024-10-22 11:19:26 +0300

    Modified DB name & project name in package.json and package-lock.json

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ export const PORT = 3000;
 
 export const DB_CONNECTION_STRING = 'mongodb://localhost:27017';
 
-export const DB_NAME = 'tech-store'; //TODO: change db name & project name in json
+export const DB_NAME = 'home-cooking-recipes'; //TODO: change db name & project name in json
 
 export const JWT_SECRET = '98132hfsdiasdfvgweuhf874o2q3hfb420f9udias08u1213';
 
